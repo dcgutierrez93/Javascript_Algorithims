7ab90e57dc2d295103ce2adb82421f6ca149fd2b
David Gutierrez <dcgutierrez93@yahoo.com>
2018-02-23 11:34:27 -0600

    Added Maps

---
+++ 07-Data-Structures/Code/maps.js
@@ -0,0 +1,11 @@
+var map = new Map();
+
+// Key and Values 
+map.set("cat", "Mr. Hyena");
+map.set("lizard", "Mr. Big Big");
+map.set("goat", "Wolf Who Ate Wall Street");
+map.set("pigeon", "Joan");
+
+console.log(map.keys());
+console.log(map.values());
+console.log(map.get("pigeon"));
